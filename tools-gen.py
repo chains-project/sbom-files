60ff3d6914d24e4da5721557fdac34e652b0e90f
Aman Sharma <mannu.poski10@gmail.com>
2023-02-21 17:09:33 +0100

    Document the final list of tools (#8)

---
+++ tools-gen.py
@@ -0,0 +1,20 @@
+import json
+
+with open('tools.json') as f:
+    tools_object = json.load(f)
+
+check = '\cmark'
+wrong = r'\xmark'
+
+tools_object = sorted(tools_object, key=lambda k: len(k['hashAlgorithms']))
+
+for i in tools_object:
+    hash_algorithms = ','.join(i['hashAlgorithms'])
+    if hash_algorithms == '':
+        hash_algorithms = None
+    
+    scope = check  if i["scope"] else wrong
+    license = check  if i["license"] else wrong
+    externalReferences = check  if i["externalReferences"] else wrong
+
+    print(f'\href{{{i["url"]}}}{{{i["name"]}}} & {i["version"]} & {i["collectionStep"]} & {hash_algorithms} & {scope} & {externalReferences}  \\\\')
