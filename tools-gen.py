6281fc86faf2a0bb32e80f7c6df83d30864d0e18
Aman Sharma <mannu.poski10@gmail.com>
2023-02-27 17:51:17 +0100

    Remove scope information

--- tools-gen.py
+++ tools-gen.py
@@ -11,10 +11,9 @@ tools_object = sorted(tools_object, key=lambda k: len(k['hashAlgorithms']))
 for i in tools_object:
     hash_algorithms = len(i['hashAlgorithms'])
     
-    scope = len(i['scope'])
     license = check  if i["license"] else wrong
     externalReferences = len(i["externalReferences"])
     canResolveTree = check  if i["canResolveTree"] else wrong
     reproducibility = check  if i["reproducibility"] else wrong
 
-    print(f'\href{{{i["url"]}}}{{{i["name"]}}} & {i["version"]} & {i["collectionStep"]} & {hash_algorithms} & {scope} & {externalReferences} & {canResolveTree} & {reproducibility}  \\\\')
+    print(f'\href{{{i["url"]}}}{{{i["name"]}}} & {i["version"]} & {i["collectionStep"]} & {hash_algorithms} & {externalReferences} & {canResolveTree} & {reproducibility}  \\\\')
